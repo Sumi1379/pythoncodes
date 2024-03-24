ba2ea17dda4215ca10da8618bf313f44e737066c
Intel <Intel@DESKTOP-8KLKQBF>
2024-03-24 16:01:58 +0530

    STRINGS

---
+++ PythonBasics/user_defined_functions/my_strings.py
@@ -0,0 +1,18 @@
+'''
+Created on 24-Mar-2024
+
+@author: Intel
+'''
+a=" SRAVANI "
+for i in a:
+    print(i)
+    
+#print(a.capitalize())  
+#print(a.casefold())
+#print(a.lower())
+print(a.upper())
+print(a.strip())
+print(a.lstrip())
+print(a.rstrip())
+
+print(a.rsplit("A"))
---
+++ PythonBasics/user_defined_functions/part_three.py
@@ -0,0 +1,8 @@
+'''
+Created on 24-Mar-2024
+
+@author: Intel
+'''
+from user_defined_functions.part_two import add
+print("This is sample")
+print(add(2, 5))
