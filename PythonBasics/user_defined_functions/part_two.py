a3d319257bcc6f93822fd769769fdff97f03a882
Intel <Intel@DESKTOP-8KLKQBF>
2024-03-24 15:03:03 +0530

    Types of argument

--- PythonBasics/user_defined_functions/part_two.py
+++ PythonBasics/user_defined_functions/part_two.py
@@ -1,5 +1,40 @@
 '''
-Created on 24-Mar-2024
-
-@author: Intel
+Types of arguments:
+1. Formal arguments
+    a. default arguments
+    b. variable length (positional) arguments
+    c. variable length keyword arguments
+2. Actual arguments
+    a. positional arguments
+    b. keyword arguments    
+    
+speacial cases:
+> passing positional arguments and keyword arguments for single function    
 '''
+def add(a=0, b=0):
+    print("a-->", a)
+    print("b-->", b)
+    c=a+b
+    print("sum-->", c)
+    
+def addition(*a):# variable length (positional) arguments
+    c=0
+    for i in a:
+        c=c+i
+    print(c)   
+def display_details(**a):# variable length keyword arguments
+    print(a)     
+    
+def mixed_v_arg(*a, **b):
+    print(a, b)    
+add(4, 2) # positional arguments
+add(b=4, a=2)# keywprd arguments
+add(8, b=6)
+#add(a=8, 6)# syntax error- positional argument can't be passed after keyword argument
+add(2)
+addition(1, 2, 3, 4, 5)
+display_details(a=1, b=2, c=3, d=4, e=5)
+mixed_v_arg(1, 2, 3, 4, c=3, d=4, e=5)
+
+
+
